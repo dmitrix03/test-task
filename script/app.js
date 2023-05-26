72da4f8f37712748a5c10a32e6626c349f5dae82
Popovdmitry17 <92853898+Popovdmitry17@users.noreply.github.com>
2023-05-26 09:07:30 +0000

    test-task

---
+++ script/app.js
@@ -0,0 +1,83 @@
+let  servicesSwiper = new Swiper('.services-swiper', {
+    slidesPerView: 4,
+    slidesPerGroup: 4,
+    speed: 1000,
+    navigation: {
+        nextEl: ".services-button-next",
+        prevEl: ".services-button-prev",
+    },
+    pagination: {
+        el: '.services-swiper-pagination',
+        type: 'bullets',
+    }
+});
+
+let  specialistsSwiper = new Swiper('.specialists-swiper', {
+    slidesPerView: 4,
+    slidesPerGroup: 4,
+    speed: 1000,
+    navigation: {
+        nextEl: ".specialists-button-next",
+        prevEl: ".specialists-button-prev",
+    },
+    pagination: {
+        el: '.specialists-swiper-pagination',
+        type: 'bullets',
+    }
+});
+
+let  informationSwiper = new Swiper('.information-swiper', {
+    slidesPerView: 4,
+    slidesPerGroup: 4,
+    speed: 1000,
+    navigation: {
+        nextEl: ".information-button-next",
+        prevEl: ".information-button-prev",
+    },
+    pagination: {
+        el: '.information-swiper-pagination',
+        type: 'bullets',
+    }
+});
+
+let  diagnosisSwiper = new Swiper('.diagnosis-swiper', {
+    slidesPerView: 4,
+    slidesPerGroup: 4,
+    speed: 1000,
+    navigation: {
+        nextEl: ".diagnosis-button-next",
+        prevEl: ".diagnosis-button-prev",
+    },
+    pagination: {
+        el: '.diagnosis-swiper-pagination',
+        type: 'bullets',
+    }
+});
+
+let  treatmentSwiper = new Swiper('.treatment-swiper', {
+    slidesPerView: 4,
+    slidesPerGroup: 4,
+    speed: 1000,
+    navigation: {
+        nextEl: ".treatment-button-next",
+        prevEl: ".treatment-button-prev",
+    },
+    pagination: {
+        el: '.treatment-swiper-pagination',
+        type: 'bullets',
+    }
+});
+
+let  portfolioSwiper = new Swiper('.portfolio-swiper', {
+    slidesPerView: 4,
+    slidesPerGroup: 4,
+    speed: 1000,
+    navigation: {
+        nextEl: ".portfolio-button-next",
+        prevEl: ".portfolio-button-prev",
+    },
+    pagination: {
+        el: '.portfolio-swiper-pagination',
+        type: 'bullets',
+    }
+});
